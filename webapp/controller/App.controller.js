0d0db198c890ab2cfe465f900dabcf404caa27cd
Local User <a.fork@t-online.de>
2025-10-16 08:56:33 +0200

    Initial commit: add project files and .gitignore

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,23 @@
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
+  function (Controller, MessageToast) {
+    "use strict";
+    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+      onClickMeButtonPress: function (oEvent) {
+        // read msg from i18n model
+        var oBundle = this.getView().getModel("i18n").getResourceBundle();
+        var sRecipient = this.getView()
+          .getModel()
+          .getProperty("/recipient/name");
+        var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+        // Show message
+        this._neueMethode();
+        MessageToast.show(sMsg + this._neueMethode());
+      },
+      _neueMethode: function () {
+        // Neue Methode
+        return "\nEigene Methode\n====================";
+      }
+    });
+  }
+);
