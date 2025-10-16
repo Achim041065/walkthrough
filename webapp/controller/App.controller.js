26f7b9901071e1ec6391a1b71aed2efe746907ff
Local User <a.fork@t-online.de>
2025-10-16 12:10:43 +0200

    Descriptors-for-Applications Text

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -16,7 +16,7 @@ sap.ui.define(
       },
       _neueMethode: function () {
         // Neue Methode
-        return "\nEigene Methode\n=======Achim Fork 16.10 =============";
+        return "\nEigene Methode\n====================";
       }
     });
   }
